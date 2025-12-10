e0977f4ea4f46ce3fc2e404984447c9b684ab611
Sachin Shrestha <sachins.newa1@gmail.com>
2025-12-10 22:01:22 +0545

    retrieve MAC address providing an IP address

---
+++ Lab8.java
@@ -0,0 +1,34 @@
+// Write a program to retrieve MAC address providing an IP address
+
+import java.net.*;
+import java.util.*;
+
+public class Lab8 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter IP Address: ");
+        String inputIP = sc.nextLine();
+
+        InetAddress ip = InetAddress.getByName(inputIP);
+        NetworkInterface ni = NetworkInterface.getByInetAddress(ip);
+
+        if (ni == null) {
+            System.out.println("No interface found for this IP!");
+            return;
+        }
+
+        byte[] mac = ni.getHardwareAddress();
+        if (mac == null) {
+            System.out.println("MAC Address not available!");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (byte b : mac) {
+            sb.append(String.format("%02X-", b));
+        }
+
+        System.out.println("MAC Address: " + sb.substring(0, sb.length() - 1));
+    }
+}
