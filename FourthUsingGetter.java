a9fe0a4eefd8846614297870577a195696be31cf
Sachin Shrestha <sachins.newa1@gmail.com>
2025-12-10 21:29:37 +0545

    lab4

---
+++ FourthUsingGetter.java
@@ -0,0 +1,41 @@
+import java.net.InetAddress;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.UnknownHostException;
+
+public class FourthUsingGetter {
+    private String ipAddress;     // property / field
+
+    // constructor sets ip
+    public FourthUsingGetter(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    // getter (this returns the ip)
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void checkIP() {
+        String ip = getIpAddress();  // calling the getter
+
+        try {
+            InetAddress address = InetAddress.getByName(ip);
+
+            if (address instanceof Inet4Address) {
+                System.out.println(ip + " is a valid IPv4 address.");
+            } else if (address instanceof Inet6Address) {
+                System.out.println(ip + " is a valid IPv6 address.");
+            } else {
+                System.out.println(ip + " is unknown IP address type.");
+            }
+        } catch (UnknownHostException e) {
+            System.out.println(ip + " is an invalid IP address.");
+        }
+    }
+
+    public static void main(String[] args) {
+        FourthUsingGetter obj = new FourthUsingGetter("192.168.88.91"); // creating object
+        obj.checkIP();   // calling method
+    }
+}
