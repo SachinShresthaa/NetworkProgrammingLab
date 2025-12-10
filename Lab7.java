27b204f9622bcc067ba826ff7430fca4a3aa9889
Sachin Shrestha <sachins.newa1@gmail.com>
2025-12-10 21:54:02 +0545

    retrieve IP address

---
+++ Lab7.java
@@ -0,0 +1,23 @@
+//Write a program to find MAC Address of the system
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+
+public class Lab7{
+    public static void main(String[] args) {
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+
+            byte[] mac = network.getHardwareAddress();
+
+            System.out.print("MAC Address: ");
+            for (int i = 0; i < mac.length; i++) {
+                System.out.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : "");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
