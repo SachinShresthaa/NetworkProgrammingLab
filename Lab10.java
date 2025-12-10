23ba3ac112d46e47e05bdc06ce2f97bc6b891651
Sachin Shrestha <sachins.newa1@gmail.com>
2025-12-11 00:00:32 +0545

    retrieve data from URL

---
+++ Lab10.java
@@ -0,0 +1,17 @@
+//Write a program to retrieve data from URL
+
+import java.net.*;
+import java.io.*;
+
+public class Lab10 {
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("https://www.google.com");
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+        br.close();
+    }
+}
